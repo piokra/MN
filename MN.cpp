b528a33070d690fbb8ba61c7ee039a73b0989fbe
student <student>
2018-04-25 17:54:40 +0200

    lab8

--- MN.cpp
+++ MN.cpp
@@ -2,9 +2,9 @@
 #include <vector>
 #include <tuple>
 #include <cmath>
-float h = 0.1;
+float h = 0.05;
 float x0 = 0, xe = 1;
-float epsilon = 0.0005;
+float epsilon = 0.0000005;
 
 std::vector<std::pair<float, float>> xy0() {
 	std::vector<std::pair<float,float>> ret;
@@ -35,19 +35,18 @@ float maxError(const std::vector<std::pair<float, float>>& left, const std::vect
 
 float jacobiStep(std::vector<std::pair<float, float>>& v) {
 	auto prev = v;
-	for (int i = 1; i<v.size()-1; ++i) {
-		v[i].second = 1./(2-10*h*h)*((1 - 5*h/2.)*prev[i+1].second+(1 + 5*h/2.)*prev[i-1].second-10*h*h*prev[i].first);
+	v[1].second = 1./(-15+50*h+120*h*h)*(-v[5].second+(6+5*h)*v[4].second + (-14 - 30*h)*v[3].second + (4+90*h)* v[2].second + (-10-15*h)*v[0].second +120*h*h*v[1].first);
+	for (int i = 2; i<v.size()-2; ++i) {
+		v[i].second = 1./(-30 + 120 * h*h)*((1.-5.*h)*v[i+2].second + (-16. + 40. * h)*v[i+1].second +
+		(-16. - 40*h)*v[i-1].second + (1 + 5*h)*v[i-2].second + 120 * h*h*v[i].first);
 	}
+	int M = v.size()-2;
+	v[M].second = 1./(-15 - 50*h + 120*h*h)*(-v[M-4].second + (6 - 5*h)*v[M-3].second + (-14 + 30 * h)*v[M-2].second + (4 - 90*h)*v[M-1].second + (-10+15*h)*v[M+1].second + 120*h*h*v[M].first);
+	
+	
 	return maxError(prev, v);
 }
 
-float gaussStep(std::vector<std::pair<float, float>>& v) {
-	auto prev = v;
-	for (int i = 1; i<v.size()-1; ++i) {
-		v[i].second = 1./(2-10*h*h)*((1 - 5*h/2.)*prev[i+1].second+(1 + 5*h/2.)*v[i-1].second-10*h*h*prev[i].first);
-	}
-	return maxError(prev, v);
-}
 
 
 int main() {
@@ -56,14 +55,5 @@ int main() {
 	while (err > epsilon) {
 		err = jacobiStep(xy);
 	}
-	print(xy);
-	
-	xy = xy0();
-	err = 1;
-	while (err > epsilon) {
-		err = gaussStep(xy);
-	}
-	print(xy);
-	
-	
+	print(xy);	
 }
